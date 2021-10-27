fcef161d33c08ee6f76ebe578755db75fe6b33c3
HanGeorge <91705358+HanGeorge@users.noreply.github.com>
2021-10-27 11:08:05 -0700

    Add files via upload

---
+++ #Halloween Art Project.py
@@ -0,0 +1,30 @@
+#Halloween Art Project
+import turtle as trtl 
+t=trtl.Turtle()
+
+import turtle
+t = turtle.Turtle()
+wn = turtle.Screen()
+wn.addshape('QdPp.gif')
+t.shape('QdPp.gif')
+wn.mainloop()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
